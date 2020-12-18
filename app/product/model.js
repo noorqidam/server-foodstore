1156be32d62a7071e3b7ed01b46d0d32aef0d08f
noorqidam <noorqidam@gmail.com>
2020-12-18 21:23:32 +0700

    add Product controller, model, router

---
+++ app/product/model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { model, Schema } = mongoose;
+
+const productSchema = Schema(
+  {
+    name: {
+      type: String,
+      minlength: [5, "Panjang nama makanan maksimal 5 karakter"],
+      required: [true, "name must be filled"],
+    },
+    description: {
+      type: String,
+      maxlength: [1000, "Panjang deskripsi maksimal 1000 karakter"],
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    image_url: String,
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+    },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Product", productSchema);
