4eba7775c26e686893e22d1e9a58d4e420f759ff
noorqidam <noorqidam@gmail.com>
2020-12-18 15:58:47 +0700

    setup env database

---
+++ app/config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  rootPath: path.resolve(__dirname, '..'),
+  serviceName: process.env.SERVICE_NAME,
+  secretKey: process.env.SECRET_KEY,
+  dbHost: process.env.DB_HOST,
+  dbPort: process.env.DB_PORT,
+  dbName: process.env.DB_NAME,
+}
